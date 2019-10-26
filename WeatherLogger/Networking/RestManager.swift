b71d39bd693823f79985ca28719d98394b29dbcb
codebendr-product-designer <codebendr@gmail.com>
2019-10-26 11:24:26 +0000

    added needed networking files

---
+++ WeatherLogger/Networking/RestManager.swift
@@ -0,0 +1,9 @@
+//
+//  RestManager.swift
+//  WeatherLogger
+//
+//  Created by codebendr on 26/10/2019.
+//  Copyright © 2019 just pixel. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherLogger/Networking/WeatherError.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherError.swift
+//  WeatherLogger
+//
+//  Created by codebendr on 26/10/2019.
+//  Copyright © 2019 just pixel. All rights reserved.
+//
+
+import Foundation
