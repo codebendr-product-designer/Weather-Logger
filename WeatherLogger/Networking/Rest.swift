e8e09935a96eb1655ecaa3fe454add8ecdd9feca
codebendr-product-designer <codebendr@gmail.com>
2019-10-26 11:28:38 +0000

    added stubs for rest and rest manager

--- WeatherLogger/Networking/Rest.swift
+++ WeatherLogger/Networking/Rest.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2019 just pixel. All rights reserved.
 //
 
+
 import Foundation
+
+struct Rest {
+    
+    private var values = [String: Any]()
+    
+    mutating func add(value: String, forKey key: String) {
+        values[key] = value
+    }
+
+    mutating func add(values: [String: Any], forKey key: String) {
+        self.values[key] = values
+    }
+    
+    mutating func add(values: [String: Any]) {
+        self.values = values
+    }
+    
+    func value(forKey key: String) -> String? {
+        return values[key] as? String
+    }
+    
+    func allValues() -> [String: Any] {
+        return values
+    }
+    
+    func totalItems() -> Int {
+        return values.count
+    }
+    
+    
+}
+
