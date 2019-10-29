84436b27f8ea78a634636be09803ae0aa898fff2
codebendr-product-designer <codebendr@gmail.com>
2019-10-29 07:04:25 +0000

    minor movement of functions

--- WeatherLogger/Controller/WeatherViewController.swift
+++ WeatherLogger/Controller/WeatherViewController.swift
@@ -20,7 +20,66 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       loadWeather(coordinate: CLLocationCoordinate2D(latitude: 35, longitude: -139))
     }
+    
+    
+    func loadWeather(coordinate: CLLocationCoordinate2D)  {
+         
+         guard let restManager = EndPoint.weather.get().restManager else { return }
+         
+         if let url = EndPoint.weather.get().url {
+             
+            restManager.parameters.add(value: "\(coordinate.latitude)", forKey: "lat")
+             restManager.parameters.add(value: "\(coordinate.longitude)", forKey: "lon")
+           //  restManager.parameters.add(value: "accra", forKey: "q")
+
+             DispatchQueue.main.async {
+             }
+             
+             restManager.request(url: url, with: .get) {
+                 results in
+                 
+                 if results.error == nil {
+                     
+                     DispatchQueue.main.async {
+                        
+                     }
+                     
+                     guard let data = results.data else {
+                         DispatchQueue.main.async {
+                            
+                         }
+                         return
+                     }
+                     
+                     WeatherClient<Weather>().decode(data: data) {
+                         result in
+                         
+                         switch result {
+                             
+                         case .success(let response):
+                            print(response)
+                             
+                         case .failure(let error):
+                             print(error)
+                             
+                         case .weatherError(let weatherError):
+                             print(weatherError)
+                             
+                         }
+                         
+                     }
+                     
+                 } else {
+                     DispatchQueue.main.async {
+                        print("general error")
+                     }
+                 }
+             }
+         }
+     }
     
 }
 
